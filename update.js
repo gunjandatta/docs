661545d2d83c5ddaff64d2be03a2b30f81befb3f
Gunjan Datta <me@dattabase.com>
2020-04-14 18:30:24 -0400

    Added the default theme.

---
+++ update.js
@@ -0,0 +1,13 @@
+var fs = require('fs')
+
+// Read the index file
+fs.readFile(someFile, 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    var result = data.replace(/string to be replaced/g, 'replacement');
+
+    fs.writeFile(someFile, result, 'utf8', function (err) {
+        if (err) return console.log(err);
+    });
+});
