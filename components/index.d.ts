62d24136bb2413a3b470e5171444c6fc79fa3537
Gunjan Datta <me@dattabase.com>
2020-04-25 16:00:10 -0400

    Removed the components directory.

--- components/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Components from "./components";
-import { Icons, IconTypes } from "./icons";
-import * as jQuery from "jquery";
-export {
-    Components,
-    Icons, IconTypes,
-    jQuery
-}
